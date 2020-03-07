111767148f3b9e92d30489f7544537f3d87ced96
Matthew Gibbs <mgibbs@lucidyne.com>
2020-03-07 13:40:01 -0800

    History mostly implemented

--- public/index.js
+++ public/index.js
@@ -1,10 +1,8 @@
 // JavaScript source code
-
 function showPosition(position) { // Test getlocation function by formating data
     x.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
 }
-
 var x = document.getElementById("demo"); // Test code
 
 var checkGeoLocButton = document.getElementById("test-geolocation"); // Tests geolocator
@@ -20,6 +18,13 @@ if (navigator.geolocation) {
 else {
     console.log("Geolocation is not supported for this Browser/OS.");
 }
-
-
-});
+})
+//const input = document.querySelector('input');
+//input.addEventListener("input",getcookieinput);
+var test= document.getElementById("cookie-id");
+//var decodecookie= decodeURIComponent(document.cookie);
+test.addEventListener("click", function(){
+  //document.cookie =document.getElementById("InputLocation");
+document.cookie = new Date() + '=' + document.getElementById("InputLocation").value;
+  var decodedcookie = document.cookie.split(";");
+})
